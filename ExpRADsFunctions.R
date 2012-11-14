0145ff466bd4295d7e26d1a48425a55bbc181e79
Sarah Supp <sarah@weecology.org>
2012-11-14 13:05:29 -0700

    Adding the correct pieces of code

---
+++ ExpRADsFunctions.R
@@ -0,0 +1,331 @@
+##### This is the source code for use with Experimental_RADs.R, the Experiments and Macroecology project
+
+library(vegan)
+library(BiodiversityR)
+library(plotrix)
+library(graphics)
+library(CCA)
+library(VGAM)
+library(nlme)
+library(lme4)
+library(languageR)
+library(poilog)
+library(scatterplot3d)
+library(hydroGOF)
+
+# All files should be organized as such:
+# RAD: SiteID | experiment/control | year | genus | species | id2species | abundance
+# Code will compare S, N and composition among treatments at each site,
+# output and compare RADs, parameters for each treatment at each site.
+
+reshape_data = function(dat){
+  # puts data in "wide" format CATEGORIZED BY SITEID for use with composition analysis
+  
+  dat2 = aggregate(list(abun = dat$abund),by=list(siteID = dat$siteID, genus = dat$genus,
+                                                  species=dat$species),FUN=sum)
+  dat2$genus_spp = paste(dat2$genus, dat2$species, sep = "_")
+  dat2 = dat2[,c(1,5,4)]
+  
+  dat3 = reshape(dat2, idvar = c('siteID'), timevar = 'genus_spp', direction = 'wide')
+  dat3[is.na(dat3)] = 0
+  
+  return(dat3)
+}
+
+# input a matrix where species are columns and rows are sites, with two sites. Calculates inverse simpson's diversity
+# and returns Simpson's evenness, where 1 represents a very even community and 0 represents a very uneven community
+SimpE = function(spp_data){
+  spp_matrix = reshape_data(spp_data)
+  D = diversity(spp_matrix[,2:ncol(spp_matrix)], index = 'invsimpson') #leave out first column, which is siteID
+  J = D / specnumber(spp_matrix[,2:ncol(spp_matrix)])
+  return (J)
+}
+
+# input a matrix where species are columns and rows are sites, with two sites. Compares the two rows using bray-curtis metric
+# returns the bray-curtis dissimilarity value, where 0 is the same and 1 is completely different
+BCdist = function(two_spp_data){
+  if (ncol(two_spp_data) == 2){
+  data = t(two_spp_data)
+  }
+  else {
+    data = reshape_data(two_spp_data) 
+    data = data[,c(2:ncol(data))]
+    data[1,] = data[1,]/sum(data[1,])
+    data[2,] = data[2,]/sum(data[2,])
+  }
+  BC = vegdist(data, method = 'bray')
+  return(BC)
+}
+
+CCA_composition = function(species_matrix, experiment){
+  # input species matrix with species as columns and rows as site variables and experiment data (in same order) as
+  # a factor variable. Makes a CCA plot with centroids and returns a significance test on the pCCA axes.
+  pcca <- cca(species_matrix ~ experiment)
+  CCA1 = as.vector(pcca$CCA$centroids[,1])
+  CCA2 = as.vector(pcca$CCA$centroids[,2])
+  pl <- plot(pcca, display = c("sp",'cn'), type = "n",xlab='', ylab='', xaxt='n')
+  sel <- orditorp(pcca, dis = 'sp', lab = F, pcol = 'gray40', pch =19)    
+  text(CCA1, CCA2, labels = c('1','2'), col='firebrick3', cex=1.25)
+  
+  pccaResult = anova(pcca)
+  return (pccaResult)
+}
+
+
+percent_unidSpp = function(siteID, dataframe) {
+  # input a numeric code for siteID and the dataframe, determines if the data can be used. 
+  # Data can be used if unidentified individuals are < 10% of the total number of individuals
+  NAindex = which(is.na(dataframe[which(dataframe[,1] == siteID),7]))
+  speciesData = as.numeric(dataframe[which(dataframe[,1] == siteID & dataframe[,6] != 0),7])
+  unidData = as.numeric(dataframe[which(dataframe[,1] == siteID & dataframe[,6] == 0),7])
+  # if there are NAs in dataframe, then P/A data, data is NOT OK
+  if (length(NAindex) > 0) {
+    return('NA')
+  }
+  #if there are no unid species, data is OK
+  else if (length(unidData) == 0){
+    return('OK')
+  }
+  # if data passes both checks, calculate percent unidentified individuals
+  else {
+    percent_unid = sum(unidData)/(sum(speciesData) + sum(unidData))
+    if (percent_unid < 0.10) {
+      return ('OK') }
+    else {
+      return (round(percent_unid * 100,2)) 
+    }
+  }
+}
+
+## Function: Calculate lambda_1 using (B.4) from Harte et al. 2008
+# This is the untruncated form of the log-series MLE
+get_lambda_sad=function(S,N){
+  if (N<=0){
+    print("Error: N must be greater than 0.")
+    return(NA)}
+  else if (S<=0){
+    print("Error: S must be greater than 0.")
+    return(NA)}
+  else if (S/N>=1){
+    print("Error: N must be greater than S.")
+    return(NA)}
+  else {
+    ## Solve for lambda 1 in Harte et al. 2008 based on (B.4)
+    y=function(x) 1/log(1/(1-x))*x/(1-x)-N/S
+    ## There should be one root between 0 and 1 since y is monotonously decreasing
+    bound=10^-10  ## Define lower and upper bound for x
+    p=uniroot(y,lower=bound,upper=1-bound)$root   ## Parameter for log-series
+    lambda_sad = p
+    return(lambda_sad)
+  }
+}
+
+## Function: chi square test of two vectors     ## FIX ME, MAKE SURE I WORK CORRECTLY!
+chi.test = function(v1, v2){
+  dat_comb=c(v1, v2)
+  q=unique(as.numeric(quantile(dat_comb, probs=seq(0, 1, 0.2)))) ## 5 bins
+  count.v1=hist(v1, breaks=q,plot=FALSE)$counts
+  count.v2=hist(v2, breaks=q,plot=FALSE)$counts
+  p.chi=chisq.test(cbind(count.v1, count.v2), simulate.p.value=TRUE)$p.value
+  return(p.chi)
+}
+
+# Function: input a vector of abundances. Calculates MLE of log-series and of poisson log-normal.
+# Gives a numeric weight for which model is better supported. Outputs 'logs' or 'logn'.
+sad_shape = function(abund){
+  #mle for untruncated log-series
+  p = get_lambda_sad(length(abund), sum(abund)) # inputs: S, N
+  lik.logs = sum(dlog(abund, prob = p, log = TRUE))
+  #mle for untruncated poisson log-normal
+  logn_vals = as.list(poilogMLE(abund, startVals = c(mean(log(abund)), sd(log(abund))))$par)
+  lik.logn = sum(log(dpoilog(abund, logn_vals$mu, logn_vals$sig)))
+  n = length(abund)
+  k1 = 1
+  k2 = 2
+  AICc.logs = 2 * k1 - 2 * lik.logs + 2 * k1 * (k1 + 1) / (n - k1 - 1)
+  AICc.logn = 2 * k2 - 2 * lik.logn + 2 * k2 * (k2 + 1) / (n - k2 - 1)
+  AICc.min = min(AICc.logs, AICc.logn)
+  weight.logs = exp(-(AICc.logs - AICc.min) / 2)
+  weight.logn = exp(-(AICc.logn - AICc.min) / 2)
+  weight = weight.logs / (weight.logs + weight.logn)  ## weight of data coming from a log-series
+  if (weight > 0.5){
+    type = 'logs'}
+  else {type = 'logn'}
+  return(list(type = type,w = weight))
+}
+
+## Function to compare the predicted abundances with empirical abundances
+## Using both chi-square test and weights b/w log-series and log-normal
+## pred is value (list) taken from SAD_rank.r
+## dat is one row in Sarah's winter_tot.csv
+dist.test=function(v1, v2){
+  require(poilog)
+  ## chi-square test
+  p.chi = chi.test(v1,v2)
+  ## weights
+  weight1 = sad_shape(v1)
+  weight2 = sad_shape(v2)
+  
+  return(list(p_chi = p.chi, con = weight1$type, exp = weight2$type))
+}
+
+# get fit to 1:1 line
+rsquare = function(con, exp){
+  return (1 - sum((con - exp) ** 2) / sum((con - mean(con)) ** 2))
+}
+
+
+RAD_plot_and_data = function(siteID1, siteID2, abundance1, abundance2, taxa){
+  #Input a numeric site ID, a list of species and a list of nonzero abundance. Makes a logscale Rank-abundance
+  #graph labeled with the string idenfier and outputs species richness, total abundance, and parameter p value.  
+  #gets poisson log-normal MLE parameters mu and sigma
+  cont_par = as.list(poilogMLE(abundance1, startVals = c(mean(log(abundance1)), sd(log(abundance1))))$par)
+  expt_par = as.list(poilogMLE(abundance2, startVals = c(mean(log(abundance2)), sd(log(abundance2))))$par)
+  #plot relative abundance, rank abunduance distributions
+  relabun1 = relabund(abundance1)
+  relabun2 = relabund(abundance2)
+  plot(NA, NA, xlab = '', ylab = '', xlim = c(0, max(c(length(abundance1), length(abundance2)))), 
+        ylim = c(0,max(c(max(relabun1),max(relabun2)))), cex.axis = 0.75)
+  lines(c(1:length(abundance1)), relabun1, type = 'l', pch = 20, cex = 2, lwd = 2)
+  lines(c(1:length(abundance2)), relabun2, type = 'l', pch = 20, cex = 2, lwd = 2, lty = 2, col = 'deeppink3')
+    legend('topright', c('control', 'experiment'), bty = 'n', lty = c(1,2), lwd = 2, col = c('black', 'deeppink3'), seg.len = 3, cex = 0.75)
+      #legend('topright', c('control', paste('mu, sigma = ', round(cont_par$mu,2), ', ', round(cont_par$sig,2)),
+      #                     'experiment', paste('mu, sigma = ', round(expt_par$mu,2),', ', round(expt_par$sig,2))), 
+      #      bty = 'n', lty = c(1,NA,2,NA), lwd = 2, col = c('black', NA, 'deeppink3', NA), cex = 0.75)
+    mtext('Relative Abundance', side = 2, line = 2, cex = 0.5)
+    mtext('Rank', side = 1, line = 2, cex = 0.5)
+    mtext(paste(taxa, ': ', siteID1, 'vs.', siteID2, sep = ' '), side = 3, line = .5, cex = 0.75)
+  
+  SAD_results = c(siteID1, cont_par$mu, cont_par$sig, siteID2, expt_par$mu, expt_par$sig)
+  return (SAD_results)
+}
+ 
+# get relative abundances from a vector of raw abundances, rounded to 3 decimal places and sorted in decreasing order
+relabund = function(abundances){
+  relativeabund = sort((abundances/sum(abundances)), decreasing = TRUE) 
+  return (relativeabund)
+}
+
+# identify the shorter vector
+shortestVector = function(v1, v2, maxLen){
+  if (length(v1) < maxLen){
+    return(list(small = v1, large = v2))
+  }
+  else {
+    return(list(large = v1, small = v2))
+  }
+}
+
+# put two relative abundances into a matrix together for euclidean distance analysis
+abundMerge = function(r1, r2){
+  ranks = c(1:max(c(length(r1),length(r2)))) # find length of longest vector
+  rSizes = shortestVector(r1, r2, max(ranks))
+  diff = max(ranks) - length(rSizes$small)  
+  rSizes$small = append(rSizes$small, rep(0,diff))
+  matrix = matrix(data = c(as.numeric(unlist(rSizes[1])), as.numeric(unlist(rSizes[2]))), nrow = max(ranks), ncol = 2)
+  return (matrix)
+}
+
+# find Euclidean distance between the RADs, returns ED rounded to 4 decimal places 
+Euclidean = function(twosite_matrix){
+  if (ncol(twosite_matrix) == 2){
+    ED = sqrt(sum((twosite_matrix[,1] - twosite_matrix[,2]) ^ 2))
+  }
+  else {
+    data = reshape_data(twosite_matrix) 
+    data = data[,c(2:ncol(data))]
+    data[1,] = data[1,]/sum(data[1,])
+    data[2,] = data[2,]/sum(data[2,])
+    data2 = t(data)
+  ED = sqrt(sum((data2[,1] - data2[,2]) ^ 2))
+  }
+  return (round(ED, 4))
+}
+
+# input a vector of values. Takes the mean (meanx) and standard deviation (sdx) of the vector, 
+#    and outputs a new vector containing the standardized values (stdz_vector)
+standardize = function(vector){
+  meanx = sum(vector)/length(vector)
+  sdx = sd(vector)
+  stdz_vector = as.numeric()
+  for (i in 1:length(vector)){
+    newVal = (vector[i] - meanx)/sdx
+    stdz_vector = append(stdz_vector, newVal)
+  }
+return (stdz_vector)
+}
+
+
+# proportion difference in S and N, with sign
+analyzeSN = function(matrix){
+  s1 = matrix[1,1]
+  s2 = matrix[1,2]
+  result = s1-s2/max(matrix) #add abs back here to get rid of sign
+  return(result)
+}
+
+
+#plots 1:1 plot of data with correct polygon and legend
+plot1to1 = function (type, controldata, experimentdata, maxpolygon, lolim, uplim) {
+  #where maxpolygon gives the maximum limit of the polygon to be drawn,
+    #lolim gives the lower limit to be used for xlim and ylim,
+    #uplim gives the upper limit to be used for xlim and ylim, and
+    #type is a character string used for the title of the graph
+  plot(NA, NA, log = 'xy', pch = 19, xlim = c(lolim, uplim), ylim = c(lolim, uplim), xlab = '', ylab = '', cex.axis = 0.75)
+  #coordinates to draw polygon fro 1.5 x's difference and 2x's difference
+  x = c(0:maxpolygon, maxpolygon:0)
+  y = c(2*(0:maxpolygon), 0.5*(maxpolygon:0))
+  polygon(x, y, border = NA, col = "lightpink")
+  y2 = c(1.5*(0:maxpolygon), (maxpolygon:0)/1.5)
+  polygon(x, y2, border = NA, col = "lightgoldenrod1")
+  abline(0, 1, lty = 2, lwd = 2, col = 'red')
+  points(controldata, experimentdata, pch = 19, xlab = 'c', ylab = '', main = '')
+  S_r2 = rsquare(controldata, experimentdata)
+  rmse = rmse(experimentdata, controldata)
+  mae = mae(experimentdata, controldata)
+  legend('topleft', c(paste('r2 = ', round(S_r2,3), sep = ''), paste('rmse = ', round(rmse,3), sep = ''), paste('mae = ', round(mae,3), sep = '')), bty = 'n', cex = 0.75)
+  mtext('Experiment', side = 2, line = 2, cex = 0.75)
+  mtext('Control', side = 1, line = 2, cex = 0.75)
+  mtext(type, side = 3, line = .25, cex = 0.75)
+}
+
+
+
+spp_dat = function(genus, species, dat) { 
+  #get ranks of top 5 species, to use inside function "top_control_spp"
+  if(nrow(dat)>0){
+    sum_abun = sum(dat[,7])
+    rank = which(dat$species==species & dat$genus == genus)
+    if(length(rank)>0){
+      rel_abun = round(dat[rank,7]/sum_abun,4)
+      spp_data = list(rank, rel_abun)}
+    if(length(rank)==0){
+      rank = 0
+      rel_abun = 0 
+      spp_data = list(rank, rel_abun)}
+  }
+  return(spp_data)}
+
+
+top_control_spp = function(control_data, experiment_data) {
+  #finds top 5 species in control community, looks at how these species reorganize in experimental communities
+  ranks_data = c("co_rank"=1,"exp_rank"=1, "co_relabun" = 1, "exp_relabun" = 1)
+
+  control_data = control_data[order(-control_data[,7]),]
+  experiment_data = experiment_data[order(-experiment_data[,7]),]
+  #identify top 3 species
+  spp = control_data[c(1:5),c(4,5)]  
+  for (s in 1:nrow(spp)) {
+    #returns list with [1]rank and [2]relative abundance
+    c_dat = spp_dat(spp[s,1], spp[s,2], control_data)    
+    e_dat = spp_dat(spp[s,1], spp[s,2], experiment_data)
+    ranks_data = rbind(ranks_data,c(as.numeric(c_dat[1]), as.numeric(e_dat[1]), as.numeric(c_dat[2]), as.numeric(e_dat[2])))
+  }
+  ranks_data = ranks_data[-1,]
+return(ranks_data)
+}
+
+plot_composition_ranks = function(ranks_data){
+
+  
+}
---
+++ expRAD_ms_script.R
@@ -0,0 +1,237 @@
+# load packages
+library(vegan)
+library(BiodiversityR)
+library(plotrix)
+library(graphics)
+library(CCA)
+library(VGAM)
+library(nlme)
+library(lme4)
+library(languageR)
+library(poilog)
+library(scatterplot3d)
+library(hydroGOF)
+library(VennDiagram)
+
+# All files should be organized as such:
+# RAD: SiteID | experiment/control | year | genus | species | id2species | abundance
+# Code will compare S, N and composition among treatments at each site,
+# output and compare RADs, parameters for each treatment at each site.
+
+##############################################################################################################################
+################ DOES STUFF #######################################
+##############################################################################################################################
+
+wd = "C:/Users//sarah//Desktop//Dropbox//Active Research Projects//ExperimentalMacroecology"
+#wd = "/Users/sarah/Desktop/WE_Dropbox_stuff/ExperimentalMacroecology/"
+setwd(wd)
+
+comms = read.csv("community_data.csv", na.strings = 'NULL')
+comms = comms[which(comms$raw_abun == 1),c(1:7)] #include only raw abundance data
+comps = read.csv("comparison_data.csv")
+names(comps)<-c('ref', 'controID','expID', 'taxa', 'experiment','unequal','type')
+comps = subset(comps, unequal == 0)
+
+#descriptive variables
+refID = c()
+cID = c()
+eID = c()
+Cshape = c()
+Eshape = c()
+CS = as.numeric()
+CN = as.numeric()
+Jc = as.numeric()
+ES = as.numeric()
+EN = as.numeric()
+Je = as.numeric()
+taxon = c()
+etype = c()
+# comparison variables
+BCrad = as.numeric()
+BCS = as.numeric()
+BCN = as.numeric()
+BCcomp = as.numeric()
+percS = as.numeric()
+percN = as.numeric()
+
+for (iRow in 1:nrow(comps)){
+  control = comps[iRow,2]  #find control in pair
+  experiment = comps[iRow,3]  # find experiment in pair
+  taxa = as.character(comps[iRow, 4])
+  type = as.character(comps[iRow, 7])
+  ref = as.character(comps[iRow,1])
+  # make sure this pair is in the subset of acceptable comm data
+  if (nrow(comms[which(comms[,1]==control),]) > 1 & nrow(comms[which(comms[,1]==experiment),]) > 1){
+    # Check that < 10% of individuals are unidentified. If meets criteria, continue
+    if (percent_unidSpp(control, comms) == "OK" & percent_unidSpp(experiment, comms) == "OK"){
+      a1 = sort(as.numeric(comms[which(comms[,1] == control & comms[,6] != 0), 7])) #vector of control abundances
+      a2 = sort(as.numeric(comms[which(comms[,1] == experiment & comms[,6] != 0), 7])) #vector of exp abundances
+      # Check that there are at least 5 species and 30 individuals in each community, If yes, proceed.
+      if (length(a1) > 4 & length(a2) > 4 & sum(a1) > 29 & sum(a2) > 29){
+        # find categorical shapes (logseries vs. lognormal)
+        d = dist.test(a1, a2)
+        # get summary statistics from comparisons
+        BCrad = append(BCrad, BCdist(abundMerge(relabund(a1), relabund(a2))))
+        BCS = append(BCS, BCdist(matrix(c(length(a1), length(a2)), nrow = 1, ncol = 2))) 
+        BCN = append(BCN, BCdist(matrix(c(sum(a1),sum(a2)), nrow = 1, ncol = 2)))
+        BCcomp = append(BCcomp, BCdist(subset(comms[which(comms$siteID == control | comms$siteID == experiment),])))
+        percS = append(percS, ((length(a2) - length(a1))/(length(a1)))*100) 
+        percN = append(percN, ((sum(a2) - sum(a1))/(sum(a1)))*100)
+        taxon = append(taxon, taxa)
+        etype = append(etype, type)
+        # record summary descriptive variables
+        refID = append(refID, ref)
+        cID = append(cID, control)
+        eID = append(eID, experiment)
+        Cshape = append(Cshape, d$con)
+        Eshape = append(Eshape, d$exp)
+        CS = append(CS, length(a1))
+        CN = append(CN, sum(a1))
+        Jc = append(Jc, round(SimpE(comms[which(comms[,1] == control),]),2))
+        ES = append(ES, length(a2))
+        EN = append(EN, sum(a2))
+        Je = append(Je, round(SimpE(comms[which(comms[,1] == experiment),]),2))
+      }}}}
+
+#collapse taxon types so there aren't so many factors
+taxon[taxon=='carabid']<-'insect'
+taxon[taxon=='lepidoptera']<- 'insect'
+taxon[taxon=='odonate']<- 'insect'
+taxon[taxon=='orthoptera']<-'insect'
+taxon[taxon=='reptile']<-'herpetofauna'
+
+#### Standardize the variables
+stdz_bc_rad = standardize(BCrad)
+stdz_bc_s = standardize(BCS)
+stdz_bc_n = standardize(BCN)
+stdz_perc_s = standardize(abs(percS))
+stdz_perc_n = standardize(abs(percN))
+stdz_bc_comp = standardize(BCcomp)
+taxon = as.factor(taxon)
+etype = as.factor(etype)
+
+# variance partitioning of variable impact on RADs, using Bray-Curtis S and N
+lm_full = lm(stdz_bc_rad ~ stdz_bc_comp + stdz_bc_s + stdz_bc_n + stdz_bc_s:stdz_bc_n)
+  r2_full = summary.lm(lm_full)$r.squared
+lm_comp = lm(stdz_bc_rad ~ stdz_bc_comp)
+  r2_comp = summary.lm(lm_comp)$r.squared
+lm_sn = lm(stdz_bc_rad ~ stdz_bc_s + stdz_bc_n + stdz_bc_s:stdz_bc_n)
+  r2_sn = summary.lm(lm_sn)$r.squared
+
+
+# variance partitioning of variable impact on RADs, using abs % difference S and N
+lm_full = lm(stdz_bc_rad ~ stdz_bc_comp + stdz_perc_s + stdz_perc_n + stdz_perc_s:stdz_perc_n)
+  r2_full = summary.lm(lm_full)$r.squared
+lm_comp = lm(stdz_bc_rad ~ stdz_bc_comp)
+  r2_comp = summary.lm(lm_comp)$r.squared
+lm_perc_sn = lm(stdz_bc_rad ~ stdz_perc_s + stdz_perc_n + stdz_perc_n:stdz_perc_s)
+  r2_perc_sn = summary.lm(lm_perc_sn)$r.squared
+  
+#root mean squared error for the variables. Usually used as standard deviation of model prediction error, but can be
+# used as an indicator of the degree of change between control (obs) and the experiment (sim)
+n_rmse = rmse(EN, CN)
+s_rmse = rmse(ES, CS)
+j_rmse = rmse(Je, Jc)
+
+#count the communities displaying various shapes. Note: doesn't take into account unique ID, could be repeats
+table(Cshape)
+table(Eshape)
+
+#### Plotting the data
+
+################## plot histograms of change, Fig 1 ###################################
+binwidth <- 0.2
+#Bray-Curtis dissimilarity: 0 = the same, 1 = completely different"
+hist(BCcomp, xlim = c(0,1), ylim = c(0,50), breaks = seq(0, 1, by = binwidth), main = "compositional similarity", 
+     col = "gray80", xlab = "", ylab = "")
+
+hist(abs(percS), xlim = c(0,120), ylim = c(0,40), breaks = seq(0, 120, by = 10), 
+     col = "gray80",  xlab = "", ylab ="")
+
+hist(abs(percN), xlim = c(0, 700), ylim = c(0,40), breaks = seq(0, 700, by = 25), 
+     col = "gray80", xlab = "", ylab = "")
+
+hist(BCrad, xlim = c(0,1), ylim = c(0,70), breaks = seq(0, 1, by = 0.11), main = "RAD similarity", 
+     xlab = "", ylab = "", col = "gray80")
+
+
+################# Figure 2. 1:1 plots of data ##########################################
+# Fig 2A - plots for N
+plot(NA, NA, pch = 19, log = 'xy', xlim = c(1,6500), ylim = c(1,6500),
+     xlab = '', ylab = '', bty = 'n')
+#x = c(0:8000, 8000:0)
+#y = c(2*(0:8000), 0.5*(8000:0))
+#polygon(x, y, border = NA, col = "lightpink")
+#y2 = c(1.5*(0:8000), (8000:0)/1.5)
+#polygon(x, y2, border = NA, col = "lightgoldenrod1")
+abline(0, 1, lty = 2, lwd = 2, col = 'red')
+points(CN, EN, pch = 19, xlim = c(30, 6500, ylim = c(30,6500), xlab = "", ylab = "", main = ""))
+N_r2 = rsquare(CN, EN)
+legend('topleft', paste('r2 = ', round(N_r2,3), sep = ''), bty = 'n', cex = 0.75)
+
+# Fig 2B - plots for S
+plot(NA, NA, log = 'xy', pch = 19, xlim = c(1,200), ylim = c(1,200),
+     xlab = '', ylab = '', bty = 'n')
+#x = c(0:300, 300:0)
+#y = c(2*(0:300), 0.5*(300:0))
+#polygon(x, y, border = NA, col = "lightpink")
+#y2 = c(1.5*(0:300), (300:0)/1.5)
+#polygon(x, y2, border = NA, col = "lightgoldenrod1")
+abline(0, 1, lty = 2, lwd = 2, col = 'red')
+points(CS, ES, pch = 19, xlim = c(5,200), ylim = c(5,200), xlab = 'c', ylab = '', main = '')
+S_r2 = rsquare(CS, ES)
+legend('topleft', paste('r2 = ', round(S_r2,3), sep = ''), bty = 'n', cex = 0.75)
+
+# Fig 2C - plots for evenness
+plot(NA, NA, xlim = c(0,1), ylim = c(0,1), pch = 19, xlab = '', ylab = '', bty = "n")
+#x = c(0:1.25, 1.25:0)
+#y = c(2*(0:1.25), 0.5*(1.25:0))
+#polygon(x, y, border = NA, col = "lightpink")
+#y2 = c(1.5*(0:1.25), (1.25:0)/1.5)
+#polygon(x, y2, border = NA, col = "lightgoldenrod1")
+abline(0,1, lty = 2, lwd = 2, col = 'red')
+points(Jc, Je, pch = 19, xlim = c(0,1), ylim = c(0,1), xlab = '', ylab = '', main = '')
+J_r2 = rsquare(Jc, Je)
+legend('topleft', paste('r2 = ', round(J_r2, 3), sep = ''), bty = 'n', cex = 0.75)
+
+#### Fig 2D.  compare relative abundance at each rank in all paired sites
+par(mfrow=c(1,1))
+plot(NA, NA, xlim = c(0,1), ylim = c(0,1), xlab = "", ylab = "", bty = "n") 
+abline(0, 1, lty = 2, col = 'red', lwd = 2)
+
+c = as.numeric()
+e = as.numeric()
+r2 = as.numeric()
+
+for (iRow in 1:nrow(comps)){
+  control = comps[iRow,2]  #find control in pair
+  experiment = comps[iRow,3]  # find experiment in pair
+  taxa = as.character(comps[iRow, 4])
+  type = as.character(comps[iRow, 7])
+  refID = as.character(comps[iRow, 1])
+  #make sure this pair is in the subset of acceptable comm data
+  if (nrow(comms[which(comms[,1]==control),]) > 1 & nrow(comms[which(comms[,1]==experiment),]) > 1){
+    #Check that < 10% of individuals are unidentified. If meets criteria, continue
+    if (percent_unidSpp(control, comms) == "OK" & percent_unidSpp(experiment, comms) == "OK"){
+      #Get a vector of all the abundances for each species in the control and in the experimental communities
+      abun_control = sort(as.numeric(comms[which(comms[,1] == control & comms[,6] != 0), 7])) 
+      abun_exprmt = sort(as.numeric(comms[which(comms[,1] == experiment & comms[,6] != 0), 7]))
+      #Check that there are at least 5 species and 30 individuals in each community, If yes, proceed.
+      if (length(abun_control) > 4 & length(abun_exprmt) > 4 & sum(abun_control) > 29 & sum(abun_exprmt) > 29){
+        relcon = relabund(abun_control) #make the lengths the same!
+        relexp = relabund(abun_exprmt)  #make the lengths the same!
+        comparison_matrix = abundMerge(relcon, relexp)
+        #add points to the open plot window
+        points(comparison_matrix[,1], comparison_matrix[,2], pch = 19) 
+        c = append(c, comparison_matrix[,1])
+        e = append(e, comparison_matrix[,2])
+        r2 = append(r2, rsquare(comparison_matrix[,1], comparison_matrix[,2]))
+      }}}}
+sigma_r2 = rsquare(c, e)
+legend('topleft', paste('r2 = ', round(sigma_r2,3), sep = ''), bty = 'n', cex = 0.75)
+
+# FIX ME  - why do I get some r2 values <0?!? FLAG AND FIGURE OUT!!!
+hist(r2[r2>0], xlim = c(0,1), ylim = c(0,75), breaks = seq(0,1, by = 0.10), col = "gray40", freq = TRUE)
+h = hist(r2[r2>0], xlim = c(0,1), ylim = c(0,1), breaks = seq(0,1, by = 0.10), col = "gray40", freq = FALSE, plot = FALSE)
+plot(h$mids, h$counts/114, pch = 19, xlim = c(0,1), ylim = c(0,1), type = 'l', lwd = 2, 
+     bty = "n", xlab = "r2 bins", ylab = "proportion")
