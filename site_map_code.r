408b56035def4b79bf29704e03974ba91e82e05e
Sarah Supp <sarah@weecology.org>
2012-12-06 13:26:35 -0700

    Added code to play with R maps
    Doesn't work yet. Would like to include a figure showing the points
    included in the study on a world map. Package "maps" seems to work, but
    doesn't loko very pretty. "ggplot2" might be the way to go - still seems
    very confusing and isn't working correctly.

---
+++ site_map_code.r
@@ -0,0 +1,46 @@
+# Rmaps for data
+library(maps)
+library(mapdata)
+library(maptools)
+library(scales)
+library(RgoogleMaps)
+library(ggplot2)
+library(RColorBrewer)
+library(mapproj)
+library(ggplot)
+
+wd = "C:\\Users\\sarah\\Documents\\GitHub\\experimental-rads"
+#wd = "/Users/sarah/Desktop/WE_Dropbox_stuff/ExperimentalMacroecology/"
+setwd(wd)
+
+sites = read.csv("sites_analysis_data.csv", na.strings = 'NULL', colClasses = "character")
+
+lat = as.numeric(sites$latitude)
+lon = as.numeric(sites$longitude)
+
+map("world", border = NA)
+axis(side = 1)
+axis(side = 2)
+points(lon, lat, col = "red", pch = 19)
+
+world<- readShapePoly("continent.shp")
+
+# Create a lat-long dataframe from the maps package
+world <- map_data("world")
+worldmap <- ggplot(world, aes(x=long, y=lat, group=group)) +
+  geom_polygon(fill="white", colour="black")
+
+# Use cartesian coordinates
+worldmap
+# With default mercator projection
+worldmap + coord_map()
+# Other projections
+worldmap + coord_map("cylindrical")
+worldmap + coord_map("azequalarea",orientation=c(-36.92,174.6,0))
+worldmap + coord_map("ortho")
+
+#add points to ggplot2 map
+pts <- data.frame(x = lon, y = lat)
+worldMap <-ggplot(world) + geom_path(aes(x,y))
+worldMap + geom_point(data = pts, aes(x, y))
+
