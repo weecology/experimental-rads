39e28d91e8f31920b1c62b405f7b992a0f53c1ee
sarahsupp <sarah@weecology.org>
2013-11-12 12:16:42 -0500

    added section for null modeling results for resubmission

--- expRADs_md_script.R
+++ expRADs_md_script.R
@@ -166,11 +166,15 @@ shapes = count_RAD_shapes(cID, eID, Cshape, Eshape)
 
 
 #fiddle plots to see if small-scale sites pick up more variability
-par(mfrow=c(1,1))
+par(mfrow=c(3,2))
 
 plot(BCcomp, m2, pch=19)
 plot(abs(percS), m2, pch=19, xlim=c(0,150))
 plot(abs(percN), m2, pch=19, xlim=c(0,300))
 plot(BCJ, m2, pch=19)
 plot(BCrad, m2, pch=19)
+
+
+#null modeling results
+
 
