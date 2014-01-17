d629da30ac27a1d852086b7381f3bc0f505a3305
sarahsupp <sarah@weecology.org>
2014-01-17 12:25:32 -0500

    added axis labels that make more sense. Center lat and long at 0

--- expRADs_md_script.R
+++ expRADs_md_script.R
@@ -230,7 +230,7 @@ site_map = base_world +
   geom_point(data=data, aes(x=lon, y=lat, col = taxa, fille = taxa, shape = taxa), cex = 5) +
   theme(legend.position = "right") + element_blank() + xlab("Longitude") + ylab("Latitude") +
   scale_colour_grey(start=0, end=0.3) + scale_shape_discrete(solid=F) + 
-  scale_x_continuous(breaks = seq(-180, 180, by=25)) +
-  scale_y_continuous(breaks = seq(-90, 90, by = 25)) +
+  scale_x_continuous(breaks = seq(-180, 180, by=30)) +
+  scale_y_continuous(breaks = seq(-90, 90, by = 15)) +
   theme(text = element_text(size=20))
 site_map
